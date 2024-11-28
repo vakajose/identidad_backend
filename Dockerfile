6e84461ac3ca954998a0fa2783514f938f05cbe9
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-11-27 20:17:26 -0400

    limpeza de comentarios y dockerfile

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Utilizar una imagen base de Python
+FROM python:3.9-slim
+
+# Establecer el directorio de trabajo
+WORKDIR /app
+
+# Copiar los archivos de requisitos
+COPY requirements.txt .
+
+# Instalar las dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copiar el resto de los archivos de la aplicación
+COPY . .
+
+# Exponer el puerto de la aplicación
+EXPOSE 8000
+
+# Crear directorio de logs
+RUN mkdir -p /app/logs
+
+# Crear directorio de img
+RUN mkdir -p /app/img
+
+# Comando para ejecutar la aplicación
+#CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["sh", "-c", "uvicorn main:app --host 0.0.0.0 --port 8000 > /app/logs/stdout.log 2>&1"]
--- routers/image.py
+++ routers/image.py
@@ -3,9 +3,7 @@ import os
 from fastapi import APIRouter, HTTPException, status
 from schemas import CedulaSchema, CedulaImageRequest
 from services import open_ai
-from pydantic import ValidationError
-from typing import Dict
-import base64
+
 
 router = APIRouter()
 
@@ -17,20 +15,9 @@ async def procesar_cedula(images: CedulaImageRequest):
         Procesa las imágenes de la cédula enviadas en base64,
         las envía al servicio de OpenAI, y devuelve el JSON resultante.
     """
-    print(images)
 
     # Validar los campos existen
     if not images.anverso or not images.reverso:
         raise HTTPException(status_code=400, detail="Faltan imágenes: anverso y/o reverso.")
-
-    # Decodificar imágenes desde base64
-    # try:
-    #     anverso = base64.b64decode(images["anverso"])
-    #     reverso = base64.b64decode(images["reverso"])
-    #     print("Imágenes decodificadas correctamente.")
-    #     print("Anverso:", images["anverso"])
-    #     print("Reverso:", images["reverso"])
-    # except Exception:
-    #     raise HTTPException(status_code=400, detail="Imágenes inválidas en formato base64.")
 
     return open_ai.analize_cedula(images)
